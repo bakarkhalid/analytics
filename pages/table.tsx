f1ed45995fb3d5ae288777d0dbe012a8a3baf41c
Abubakar Khalid <28635046+bakarkhalid@users.noreply.github.com>
2023-07-03 17:52:34 +0500

    fix teh table content

--- pages/table.tsx
+++ pages/table.tsx
@@ -2,35 +2,40 @@ import GenericTable from '@/Components/Table';
 import React from 'react'
 
 const table = () => {
-          const columns = [
-  {
-    title: 'Keyword',
-    dataIndex: 'name',
-    key: 'Keyword',
-    width: 100,
-  },
-  {
-    title: 'Receving Platform',
-    dataIndex: 'Receving Platform',
-    key: 'Receving Platform',
-    width: 100,
-  },
-  {
-    title: 'Recevied From User',
-    dataIndex: 'Recevied From User',
-    key: 'Recevied From User',
-    width: 200,
-  },
-  {
-    title: 'Date',
-    dataIndex: '',
-    key: 'Date',
-  },
-];
+  const columns = [
+    {
+      title: 'Keyword',
+      dataIndex: 'Keyword',
+      key: 'Keyword',
+    },
+    {
+      title: 'Receving Platform',
+      dataIndex: 'receving',
+      key: 'receving',
+    },
+    {
+      title: 'Recevied From User',
+      dataIndex: 'recevied',
+      key: 'recevied',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'date',
+      key: 'date',
+    },
+  ];
 
-const data = [
-  { Keyword: 'Jack', ["Receving Platform"]: 28, ["Recevied From User"]: 'some where', ["Date"]: '1' },
-];
+  const data = [
+    { Keyword: 'LordBGR', receving: "Telegram", recevied: '@username / +01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "WhatsApp", recevied: '+01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "Email", recevied: 'username@mail.com ', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "WhatsApp", recevied: '+01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "Telegram", recevied: '@username / +01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "WhatsApp", recevied: '+01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "Email", recevied: 'username@mail.com ', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "WhatsApp", recevied: '+01 234 5678', date: 'Sat May 6 2023' },
+    { Keyword: 'LordBGR', receving: "Email", recevied: 'username@mail.com ', date: 'Sat May 6 2023' },
+  ];
   return (
     <GenericTable columns={columns} data={data} />
   )
